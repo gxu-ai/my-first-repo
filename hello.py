3f445e2ce2f8734849a9e0c3dd21d86d1cfa35f6
Gang Xu <gxu@pitp.ca>
2025-11-11 15:09:55 -0500

    Updated hello.py message

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 # My first GitHub repository example
 
 def main():
-    print("Hello, world! This is my first GitHub repo.")
+    print("Hello, world! This is my updated GitHub repo.")
 
 if __name__ == "__main__":
     main()
